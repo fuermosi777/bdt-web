2050e2ae0b8d7057a7f188cc19f16a0f527174a2
Hao Liu <fuermosi777@users.noreply.github.com>
2023-12-15 21:16:50 -0800

    Add sidebar

---
+++ src/components/_Template.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./_Template.scss";
+
+function _Template() {
+  return (
+    <div className="_Template">
+      
+    </div>
+  );
+}
+
+export default _Template;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 // import logo from "./logo.svg";
 import "./App.scss";
+import Sidebar from "../components/Sidebar.tsx";
 
 function App() {
   return (
     <div className="App">
-      
+      <Sidebar/>
     </div>
   );
 }
