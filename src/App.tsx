1748f1b878dd9737d44de4013509320b1ad1d435
Hao Liu <fuermosi777@users.noreply.github.com>
2023-12-15 19:34:09 -0800

    add ts support

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          白酒定制！
-        </p>
+        <p>白酒定制！</p>
         <a
           className="App-link"
           href="https://reactjs.org"
