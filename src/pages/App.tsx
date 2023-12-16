639079b95d6addb588e486d5c2deb234b7701e9c
Hao Liu <fuermosi777@users.noreply.github.com>
2023-12-15 21:40:15 -0800

    Add three

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,11 +3,13 @@ import React from "react";
 // import logo from "./logo.svg";
 import "./App.scss";
 import Sidebar from "../components/Sidebar.tsx";
+import ThreeDPreviewer from "../components/ThreeDPreviewer.tsx";
 
 function App() {
   return (
     <div className="App">
       <Sidebar/>
+      <ThreeDPreviewer/>
     </div>
   );
 }
