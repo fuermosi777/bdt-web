6c766f8713df8fa4011270767ce02403af3d60f9
Hao Liu <fuermosi777@users.noreply.github.com>
2023-12-17 21:31:45 -0800

    WIP editor

--- src/pages/Designer.tsx
+++ src/pages/Designer.tsx
@@ -1,19 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import "./Designer.scss";
 import Sidebar from "../components/Sidebar.tsx";
-import ThreeDPreviewer from "../components/ThreeDPreviewer.tsx";
 import MainPanel from "../components/MainPanel.tsx";
-import ExpBottlePreview from "../components/ExpBottlePreview.tsx";
-
-import ToggleButtonGroup from "@mui/joy/ToggleButtonGroup";
-import Button from "@mui/joy/Button";
 import Header from "../components/Header.tsx";
 import Content from "../components/Content.tsx";
-
+import Konva from "konva";
 
 // The entry point for the packaging designer tool.
 const Designer = () => {
+  const canvasRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const stage = new Konva.Stage({
+      container: canvasRef.current,
+      width: canvasRef.current.offsetWidth,
+      height: canvasRef.current.offsetHeight,
+      draggable: true,
+    });
+
+    const layer = new Konva.Layer();
+    stage.add(layer);
+
+    const background = generateBackground(stage);
+    layer.add(background);
+  }, []);
+
   return (
     <div className="Designer">
       <Header />
@@ -22,11 +36,35 @@ const Designer = () => {
           <p></p>
         </Sidebar>
         <MainPanel>
-          <p></p>
+          <div
+            className="canvas"
+            ref={canvasRef}
+            style={{
+              width: 800,
+              height: 600,
+            }}
+          ></div>
         </MainPanel>
       </Content>
     </div>
   );
 };
+
+// Generate a Konva.Rect of background so that it can be added to the layer/stage.
+function generateBackground(stage: Konva.Stage): Konva.Rect {
+  const background = new Konva.Rect({
+    x: 0,
+    y: 0,
+    width: stage.width(),
+    height: stage.height(),
+    fill: "#FFFFFF",
+    listening: false,
+  });
+  stage.on("dragmove", () => {
+    background.absolutePosition({ x: 0, y: 0 });
+  });
+
+  return background;
+}
 
 export default Designer;
