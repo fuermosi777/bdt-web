d885caf876c07010aaa53eafe2c02ec0e414fb9f
Hao Liu <fuermosi777@users.noreply.github.com>
2023-12-24 21:42:00 -0800

    add transformer

--- src/components/PackageEditor.tsx
+++ src/components/PackageEditor.tsx
@@ -7,7 +7,7 @@ import ThreeDPreviewer from "./ThreeDPreviewer.tsx";
 const PackageEditor = (props: {
   asset: PackageAsset;
   onEdited: (imageData: ThreeDPreviewer.ImageData) => void;
-  hidden: boolean
+  hidden: boolean;
 }) => {
   const canvasRef = useRef<HTMLDivElement>(null);
   const { asset } = props;
@@ -86,6 +86,54 @@ const PackageEditor = (props: {
         }
       }
     }
+
+    // Add transformer in the end so that it is always on top.
+    let transformer = new Konva.Transformer();
+    layer.add(transformer);
+
+    stage.on('mousedown touchstart', (e) => {
+      // do nothing if we mousedown on any shape
+      if (e.target !== stage) {
+        return;
+      }
+      e.evt.preventDefault();
+    });
+
+
+    // clicks should select/deselect shapes
+    stage.on('click tap', function (e) {
+      // if click on empty area - remove all selections
+      if (e.target === stage) {
+        transformer.nodes([]);
+        return;
+      }
+
+      // do nothing if clicked NOT on our rectangles
+      // if (!e.target.hasName('rect')) {
+      //   return;
+      // }
+
+      // do we pressed shift or ctrl?
+      const metaPressed = e.evt.shiftKey || e.evt.ctrlKey || e.evt.metaKey;
+      const isSelected = transformer.nodes().indexOf(e.target) >= 0;
+
+      if (!metaPressed && !isSelected) {
+        // if no key pressed and the node is not selected
+        // select just one
+        transformer.nodes([e.target]);
+      } else if (metaPressed && isSelected) {
+        // if we pressed keys and node was selected
+        // we need to remove it from selection:
+        const nodes = transformer.nodes().slice(); // use slice to have new copy of array
+        // remove node from array
+        nodes.splice(nodes.indexOf(e.target), 1);
+        transformer.nodes(nodes);
+      } else if (metaPressed && !isSelected) {
+        // add the node into selection
+        const nodes = transformer.nodes().concat([e.target]);
+        transformer.nodes(nodes);
+      }
+    });
   }, []);
 
   return (
